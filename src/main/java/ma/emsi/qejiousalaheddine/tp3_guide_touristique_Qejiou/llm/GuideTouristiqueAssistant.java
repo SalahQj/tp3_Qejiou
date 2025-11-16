55de8f324416c3335a29fc7315cd14c63b38e14e
SalahQj <qj.salaheddine@gmail.com>
2025-11-16 19:40:35 +0100

    création de l'interface GuideTouristiqueAssistant

---
+++ src/main/java/ma/emsi/qejiousalaheddine/tp3_guide_touristique_Qejiou/llm/GuideTouristiqueAssistant.java
@@ -0,0 +1,20 @@
+package ma.emsi.qejiousalaheddine.tp3_guide_touristique_Qejiou.llm;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+
+public interface GuideTouristiqueAssistant {
+
+    @SystemMessage("""
+        Tu es un guide touristique expert. Réponds UNIQUEMENT avec un objet JSON
+        valide, basé sur le schéma demandé.
+        N'ajoute AUCUN texte avant ou après le JSON, et n'utilise PAS de Markdown.
+        Le JSON doit avoir exactement 3 clés :
+        1. 'ville_ou_pays' (le nom du lieu demandé)
+        2. 'endroits_a_visiter' (une liste de 2 noms de lieux)
+        3. 'prix_moyen_repas' (une chaîne de caractères avec le prix ET la devise, ex: "20 EUR")
+        """)
+    @UserMessage("Donne-moi les informations touristiques pour {{lieu}}")
+    InfosTouristiques chat(@V("lieu") String lieu); // Retourne directement le record
+}
